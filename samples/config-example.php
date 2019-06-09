ca7844af2358d24e4b6055af627a54f1ccd2fbf8
Adam Faryna <adamfaryna@appdy.net>
2019-06-09 11:31:04 +0100

    init

---
+++ samples/config-example.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = [
+  'smtp_host' => '',
+  'smtp_auth' => true,
+  'smtp_port' => '25',
+  'smtp_secure' => 'tls',
+  'smtp_username' => '',
+  'smtp_password' => '',
+  'smtp_from' => '',
+  'smtp_fromname' => ''
+];
+
+?>
---
+++ wc-email-config.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Plugin Name: WC Email Config
+ * Plugin URI: https://github.com/adamfaryna/wc-email-config
+ * Description: Wordpress Woocommerce WC_Email config plugin
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Author: Appdy LTD
+ * Author URI: https://github.com/adamfaryna
+ */
+
+defined( 'ABSPATH' ) or die('No script kiddies please!');
+
+class WC_Email_Config {
+
+  private static $instance;
+
+  function __construct() {
+    add_action('phpmailer_init', [$this, 'configure_mailer']);
+    add_action('phpmailer_init', [$this, 'fix_multipart_issue']);
+    add_action('wp_mail_failed', [$this, 'handle_error']);
+  }
+
+  function __destruct() {
+    remove_action('phpmailer_init', [$this, 'configure_mailer']);
+    remove_action('wp_mail_failed', [$this, 'handle_error']);
+  }
+
+  static function instance() {
+    if (isset($this->$instance)) {
+      $this-$instance = new WC_Email_Config();
+    }
+
+    return $instance;
+  }
+
+  function configure_mailer() {
+    $config_path = apply_filters('appdy_wc_email_config_path', null);
+
+    if (file_exists($config_path)) {
+      require_once($config_path);
+      $mailer->isSMTP();
+      $mailer->Host       = $config['smtp_host'];
+      $mailer->SMTPAuth   = $config['smtp_auth'];
+      $mailer->Port       = $config['smtp_port'];
+      $mailer->SMTPSecure = $config['smtp_secure'];
+      $mailer->Username   = $config['smtp_username'];
+      $mailer->Password   = $config['smtp_password'];
+      $mailer->From       = $config['smtp_from'];
+      $mailer->FromName   = $config['smtp_fromname'];
+      $mailer->SMTPDebug  = 2;
+      $mailer->CharSet    = 'utf-8';
+
+    } else {
+      error_log('wc-email-config: no config file found!');
+    }
+  }
+
+  function handle_error() {
+    error_log('wp_mail failed: ' . $error->get_error_message());
+  }
+
+  function fix_multipart_issue($phpmailer) {
+    if (empty($phpmailer->AltBody)) {
+      $phpmailer->AltBody = strip_tags($phpmailer->Body);
+    }
+  }
+}
+
+WC_Email_Config::instance();
+
+?>
